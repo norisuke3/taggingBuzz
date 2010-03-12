8314de07859bfae3c8d493ea645460357da8c57e
Noriaki Hamamoto <norisuke3@gmail.com>
2010-03-11 22:09:06 -0400

    modified popup to show mock page.

--- popup.js
+++ popup.js
@@ -1,4 +1,28 @@
 $(function(){
-  $("div").text("Hello world");
+  var tags = JSON.parse(localStorage.tag_list);
+
+  tags.forEach(function(tag){
+    var links = JSON.parse(localStorage.getItem(tag));
+      
+    var content = jQuery("<content>");
+    links.forEach(function(link){
+      content
+      .append("<span>")
+	.find("span:last")
+	.text(tag + ": ")
+	.append("<a>")
+	  .find("a")
+	  .attr("href", link)
+	  .attr("target", "_blank")
+	  .text(tag + ": " + link.substr(0, 10))
+	.end()
+	.append("<br>");
+    });
+		 
+    $("div#top")
+    .append("<div class=" + tag + ">")
+      .find("div." + tag)
+      .append(content.children());
+  });
 });
 
