d20b8812a5949a21a4ffee5df38893969c6bb1b4
Noriaki Hamamoto <norisuke3@gmail.com>
2010-03-13 21:06:52 -0400

    added a couple of plugin functions, attrCopy(), dbg(), completeURL and attach().

--- lib.js
+++ lib.js
@@ -18,3 +18,67 @@ jQuery.fn.findAll = function(func){
   
   return result || $([]);
 };
+
+
+jQuery.fn.attrCopy = function(attr1, attr2){
+  this.each(function(){
+    $(this).attr(attr2, $(this).attr(attr1));
+  });
+  
+  return this;
+}
+
+jQuery.fn.dbg = function(){
+  debugger;
+  
+  return this;
+}
+
+jQuery.fn.complementURL = function(options){
+  var setting = $.extend({
+    protocol: "http",
+    domain: "",
+    path: null
+  }, options);
+
+  var url_base = setting.protocol + "://" + 
+		 ( setting.domain.slice(-1) == "/" ? setting.domain : setting.domain + "/" ) + 
+		 ( setting.path || "" );
+  
+  var link_tag = {
+    a  : "href",
+    img: "src"
+  };
+  
+  this.each(function(){
+    var attr = link_tag[this.tagName.toLowerCase()];
+	      
+    if(attr && $(this).attr(attr).slice(0, 1) == "/"){
+      $(this).attr(attr, url_base + $(this).attr(attr).substr(1));
+    }
+  });
+
+  return this;
+}
+
+
+jQuery.fn.attach = function(elmExp){
+  var tagInfo;
+  var tag;
+  
+  if( elmExp.indexOf("#") != -1 ){
+    tag = elmExp.split("#");
+    tagInfo = { tag: tag[0], id: tag[1] };
+  } else  if( elmExp.indexOf(".") != -1 ){
+    tag = elmExp.split(".");
+    tagInfo = { tag: tag[0], classes: tag[1] };
+  } else {
+    tagInfo = { tag: elmExp };
+  }
+
+  return this.append(
+    "<" + tagInfo.tag + ( tagInfo.id ? " id='" + tagInfo.id + "'" : "") + 
+                        ( tagInfo.classes ? " class='" + tagInfo.classes + "'" : "") + 
+    ">"
+  ).find(tagInfo.tag + ":last");
+}
